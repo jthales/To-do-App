d3477ee4af9394b4b4dc4e97aa70fcd109fbe04b
Thales <thaleshenrique38@live.com>
2019-01-31 09:37:06 -0200

    backend aula 38

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,12 +1,14 @@
 const port = 3003
 
-const bodyParser = requie('body-parser')
-const express = requie('express')
+const bodyParser = require('body-parser')
+const express = require('express')
 const server = express()
 
 server.use(bodyParser.urlencoded({ extended: true}))
 server.use(bodyParser.json())
 
-server.listem(port, function() {
+server.listen(port, function() {
     console.log(`BACKEND is runnign on port ${port}.`)
 })
+
+module.exports = server

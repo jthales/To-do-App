11dd69ba982b9eb8b0d1ede6cb90856978c6c75e
Thales <thaleshenrique38@live.com>
2019-02-12 20:19:43 -0200

    att jsx aulas todo

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,5 +1,5 @@
 const webpack = require('webpack')
-const ExtractTextPlugin = require(extract-text-webpack-plugin)
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     entry: './src/index.jsx',

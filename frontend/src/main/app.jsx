607123d771c463e8dc28df93d753d2d87d7108f6
Thales <thaleshenrique38@live.com>
2019-02-18 20:57:38 -0300

    Atualização finalização do Todo-App

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,6 +1,8 @@
 import 'modules/bootstrap/dist/css/bootstrap.css'
 import 'modules/font-awesome/css/font-awesome.css'
 
+import '../template/custom.css'
+
 import React from 'react'
 import Routes from './routes'
 import Menu from '../template/menu'
